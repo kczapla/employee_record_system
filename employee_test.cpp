18cb45d53ff32e52e68f7c7375541dfb1711950e
kczapla <k.czapla68@gmail.com>
2018-10-01 18:35:01 +0200

    init commit

---
+++ employee_test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "employee.hpp"
+
+
+using namespace std;
+using namespace Records;
+
+
+int main () {
+    cout << "Testing the employee class." << endl;
+    Employee emp;
+    emp.setFirstName("John");
+    emp.setLastName("Doe");
+    emp.setEmployeeNumber(71);
+    emp.setSalary(50000);
+    emp.promote();
+    emp.promote(50);
+    emp.hire();
+    emp.display();
+    return 0;
+}
